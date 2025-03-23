b5f0e2be4ef10b3e51464e5f5f97568ac51a0ac3
Alexis Degraeve <alexis.degraeve@hotmail.com>
2025-03-23 20:27:14 +0100

    change stage style

--- qpuc/src/components/stage/stage.jsx
+++ qpuc/src/components/stage/stage.jsx
@@ -2,9 +2,24 @@ import './stage.scss'
 
 const Stage = ({level, active}) => {
     return (
-        <div className={active ? 'active' : '' }>
-            STAGE
-            { level }
+        <div >
+            <div>
+            <svg height="280" width="360" xmlns="http://www.w3.org/2000/svg" className={'stage ' + (active ? 'active' : '')}>
+                <defs>
+                    <linearGradient id="header-shape-gradient" x1="0%" y1="0%" x2="0%" y2="100%">
+                    <stop offset="0%" className='stop1'/>
+                    <stop offset="50%" className='stop2'/>
+                    <stop offset="100%" className='stop1'/>
+                    </linearGradient>
+                </defs>
+                <polygon points="150,15 258,77 258,202 150,265 42,202 42,77" fill="url(#header-shape-gradient)"  />
+                <text x="50%" y="50%" class="heavy">{ level }</text>
+                Sorry, your browser does not support inline SVG.
+                </svg>
+
+            </div>
+            
+            
         </div>
     );
 }
