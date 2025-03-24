e07fc298867036903079eed924467e51c01875c7
Alexis Degraeve <alexis.degraeve@hotmail.com>
2025-03-24 14:43:09 +0100

    change design question

--- qpuc/src/components/question/question.jsx
+++ qpuc/src/components/question/question.jsx
@@ -1,56 +1,82 @@
-import { useEffect, useRef, useState } from 'react'
-import './question.scss'
+import { useEffect, useRef, useState } from "react";
+import "./question.scss";
 
-const Question = ({showFront, question, answer, points, onCountScore}) => {
-    const [myAnswer, setMyAnswer] = useState('');
-    const [correct, setCorrect] = useState();
-    const inputAnswer = useRef(null)
+const Question = ({ showFront, question, answer, points, onCountScore }) => {
+  const [myAnswer, setMyAnswer] = useState("");
+  const [correct, setCorrect] = useState();
+  const inputAnswer = useRef(null);
 
-    useEffect (() => {
-        inputAnswer.current.focus();
-    })
+  useEffect(() => {
+    inputAnswer.current.focus();
+  });
 
-    const handleMyAnswer = (event) => {
-        setMyAnswer(event.target.value)
+  const handleMyAnswer = (event) => {
+    setMyAnswer(event.target.value);
+  };
+
+  const checkAnswer = () => {
+    if (myAnswer.toLowerCase() == answer.toLowerCase()) {
+      onCountScore(points + 3);
+      setCorrect(true);
+    } else {
+      setCorrect(false);
     }
+  };
 
-    const checkAnswer = () => {
-            if(myAnswer.toLowerCase() == answer.toLowerCase()) {
-                onCountScore(points + 3)
-                setCorrect(true);
-            } else {
-                setCorrect(false);
-            }
+  const handleKeyDown = (e) => {
+    if (e.key == "Enter") {
+      checkAnswer();
     }
+  };
 
-    const handleKeyDown = (e) => {
-        if (e.key ==  "Enter") {
-            checkAnswer()
-        }
-    }
+  return (
+    <>
+      <div className="questioncard">
+        <div className={"frontcard " + (showFront ? "" : "nonactive")}>
+          AVANT
+          <span className="champion-text">Champion</span>
+        </div>
 
-
-    return (<>
-            <div className="questioncard">
-                <div className ={'frontcard '+ (showFront ? '' : 'nonactive')}>
-                    AVANT
-                    <span className="champion-text">Champion</span>
-                </div>
-
-                <div className ={'backcard '+ (!showFront ? '' : 'nonactive')}>
-                    ARRIERE
-                    {question}
-                    {answer}
-                    {points}
-                    <input ref={inputAnswer} type="text" value={myAnswer} onChange={handleMyAnswer} placeholder='Your answer' onKeyDown={handleKeyDown}></input>
-                    <button className="btn btn-primary" onClick={checkAnswer}>
-                        VALID!
-                    </button>
-                    YOUR ANSWER IS CORRECT { correct ? 'YES ' : 'NO'}
-                    <br />
-                </div>
+        <div className={"backcard " + (!showFront ? "" : "nonactive")}>
+          <h5>{question}</h5>
+          {answer}
+          {points}
+          <form>
+            <div class="input-group mb-3">
+              <input
+                className="form-control"
+                ref={inputAnswer}
+                type="text"
+                value={myAnswer}
+                onChange={handleMyAnswer}
+                placeholder="Your answer"
+                onKeyDown={handleKeyDown}
+              ></input>
+              <button
+                className="btn btn-primary"
+                type="button"
+                id="button-addon2"
+                onClick={checkAnswer}
+              >
+                OK <i class="bi bi-check"></i>
+              </button>
             </div>
-    </>);
-}
+          </form>
+          {correct ? (
+            <div class="alert alert-success" role="alert">
+              YOUR ANSWER IS CORRECT
+            </div>
+          ) : (
+            <div class="alert alert-warning" role="alert">
+              YOUR ANSWER IS NOT CORRECT
+            </div>
+          )}
 
-export default Question
+          <br />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Question;
